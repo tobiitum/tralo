3212cd45cda69e872173f5b1b517c5b69d4f8050
tobiitum <tbs.kling@gmail.com>
2022-08-15 20:12:09 +0200

    Initial Commit

---
+++ tralo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tralo
+//
+//  Created by Tobias Klingenberg on 15.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tralo/traloApp.swift
@@ -0,0 +1,17 @@
+//
+//  traloApp.swift
+//  tralo
+//
+//  Created by Tobias Klingenberg on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct traloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
